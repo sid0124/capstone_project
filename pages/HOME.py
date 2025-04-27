c67a54e1cf9b807548260fc8a49c758236c247ae
sid0124 <113914986+sid0124@users.noreply.github.com>
2025-04-27 23:43:19 +0530

    Add files via upload

---
+++ pages/HOME.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to World of Realestate! 👋")
+
+st.sidebar.success("Select a demo above.")
